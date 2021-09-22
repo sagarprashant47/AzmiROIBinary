be810a84a774adf2537b8d3c54e2a21f72ff219c
Prashant Sagar <prashant.sagar@collabera.com>
2021-09-22 23:37:57 +0530

    TransferFund Security pwd filed added

--- AngularApp/src/app/views/base/transferfund.component.ts
+++ AngularApp/src/app/views/base/transferfund.component.ts
@@ -16,6 +16,7 @@ export class TransferFundComponent {
   CustomerEmail="";
   CustomerInfoModel = { BitcoinAddress : "",AvailableBalance:""};
   Amount=0;
+  securityPwd="";
   ngOnInit(): void {
     $('.loaderbo').show();
     this.customerservice.GetCustomerInfo(this.CustomerId)
@@ -29,13 +30,14 @@ export class TransferFundComponent {
   }
 
   transferfund(){
-    let model = { CustomerId : this.CustomerId,CustomerEmail:"",Amount:0 };
+    let model = { CustomerId : this.CustomerId,CustomerEmail:"",Amount:0 ,SecurityPwd:""};
     model.CustomerEmail = this.CustomerEmail;
     model.Amount = this.Amount;
     model.CustomerId = this.CustomerId;
+    model.SecurityPwd = this.securityPwd;
 
     if(model.CustomerEmail == ""){
-      this.toastr.error("Please enter email");
+      this.toastr.error("Please enter Username");
       return;
     }
     if(model.Amount <= 0){
@@ -46,26 +48,52 @@ export class TransferFundComponent {
       this.toastr.error("Maximum Transfer 100$ Per Day.");
       return;
     }
+    if(model.SecurityPwd == ""){
+      this.toastr.error("Please enter Security Password");
+      return;
+    }
 
     $('.loaderbo').show();
-    this.commonservice.TransferFund(model)
-    .subscribe(
-      res => {
-        if(res.Message == "success"){
-          this.CustomerEmail= "";
-          this.Amount = 0;
-          this.toastr.success("Transfer Successfull");
-          this.ngOnInit();
+    let SecModel = { CustomerId : this.CustomerId,CustomerEmail:"",SecurityPassword:model.SecurityPwd};
+     this.commonservice.GetCustomerSecurityPwd(SecModel)
+      .subscribe(
+        res =>{
+          if(res.Message === "success"){
+            debugger
+            if(res.data.indexOf('Invalid') !== -1){
+              this.toastr.error("Please Enter Correct Security Password")
+            }
+            else{
+              this.commonservice.TransferFund(model)
+              .subscribe(
+                res => {
+                  if(res.Message == "success"){
+                    this.CustomerEmail= "";
+                    this.Amount = 0;
+                    this.toastr.success("Transfer Successfull");
+                    this.ngOnInit();
+                  }
+                  else{
+                    this.toastr.success(res.Message);
+                  }
+                  $('.loaderbo').hide();
+                },
+                err => {
+                  this.toastr.error("Something went wrong");
+                  $('.loaderbo').hide();
+                }
+              )      
+            }
+          }
+          else{
+            this.toastr.error(res.Message)
+          }
+          $('.loaderbo').hide();
+        },
+        err =>{
+          this.toastr.error("Something went wrong");
+          $('.loaderbo').hide();
         }
-        else{
-          this.toastr.success(res.Message);
-        }
-        $('.loaderbo').hide();
-      },
-      err => {
-        this.toastr.error("Something went wrong");
-        $('.loaderbo').hide();
-      }
-    )
+      ) 
   }
 }
